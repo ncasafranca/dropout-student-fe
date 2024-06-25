66d73e463e46d11d6bbfa27aef7c194c92603ed9
Niel Casafranca <u202014690@upc.edu.pe>
2024-06-25 10:40:17 -0500

    update dependencies and rename project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'churn-customer-fe'`, () => {
+  it(`should have as title 'dropout-student-fe'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('churn-customer-fe');
+    expect(app.title).toEqual('dropout-student-fe');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('churn-customer-fe app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('dropout-student-fe app is running!');
   });
 });
